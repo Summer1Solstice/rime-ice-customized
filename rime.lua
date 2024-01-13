7043976ad0330e359519a2814e740564cabac014
laptop <46267607+Summer1Solstice@users.noreply.github.com>
2024-01-13 16:54:19 +0800

    更新：同步rime-ice

--- rime.lua
+++ rime.lua
@@ -5,7 +5,7 @@
 
 -- processors:
 
--- 以词定字，可在 default.yaml key_binder 下配置快捷键，默认为左右中括号 [ ]
+-- 以词定字，可在 default.yaml → key_binder 下配置快捷键，默认为左右中括号 [ ]
 select_character = require("select_character")
 
 
@@ -14,6 +14,9 @@ select_character = require("select_character")
 
 -- 日期时间，可在方案中配置触发关键字。
 date_translator = require("date_translator")
+
+-- 农历，可在方案中配置触发关键字。
+lunar = require("lunar")
 
 -- Unicode，U 开头
 unicode = require("unicode")
@@ -26,7 +29,8 @@ number_translator = require("number_translator")
 -- filters:
 
 -- 错音错字提示
--- corrector = require("corrector")
+-- 关闭此 Lua 时，同时需要关闭 translator/spelling_hints，否则 comment 里都是拼音
+corrector = require("corrector")
 
 -- v 模式 symbols 优先（全拼）
 v_filter = require("v_filter")
@@ -34,7 +38,7 @@ v_filter = require("v_filter")
 -- 自动大写英文词汇
 autocap_filter = require("autocap_filter")
 
--- 降低部分英语单词在候选项的位置，可在方案中配置要降低的单词
+-- 降低部分英语单词在候选项的位置，可在方案中配置要降低的模式和单词
 reduce_english_filter = require("reduce_english_filter")
 
 
@@ -50,6 +54,9 @@ reduce_english_filter = require("reduce_english_filter")
 -- long_word_filter:
 --   count: 2
 --   idx: 4
+--
+-- 使用请注意：之前有较多网友反应有内存泄漏，优化过一些但还是偶尔有较高的内存，但并不卡顿也不影响性能，重新部署后即正常
+-- 如果要启用，建议放到靠后位置，最后一个放 uniquifier，倒数第二个就放 long_word_filter
 long_word_filter = require("long_word_filter")
 
 -- 中英混输词条自动空格
@@ -68,7 +75,7 @@ t9_preedit = require("t9_preedit")
 -- 在 engine/filters 增加 - lua_filter@is_in_user_dict
 -- 在方案里写配置项：
 -- is_in_user_dict: true     为输入过的内容加星号
--- is_in_user_dict: flase    为未输入过的内容加星号
+-- is_in_user_dict: false    为未输入过的内容加星号
 is_in_user_dict = require("is_in_user_dict")
 
 -- 词条隐藏、降频
@@ -77,5 +84,6 @@ is_in_user_dict = require("is_in_user_dict")
 -- 在 key_binder 增加快捷键：
 -- turn_down_cand: "Control+j"  # 匹配当前输入码后隐藏指定的候选字词 或候选词条放到第四候选位置
 -- drop_cand: "Control+d"       # 强制删词, 无视输入的编码
+-- get_record_filername() 函数中仅支持了 Windows、macOS、Linux
 cold_word_drop_processor = require("cold_word_drop.processor")
 cold_word_drop_filter = require("cold_word_drop.filter")
