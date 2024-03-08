81c55effddc419d81061488f2a546de7574bb3c9
laptop <46267607+Summer1Solstice@users.noreply.github.com>
2024-03-08 08:53:49 +0800

    无

--- lua/cold_word_drop/turndown_freq_words.lua
+++ lua/cold_word_drop/turndown_freq_words.lua
@@ -1,5 +1,6 @@
 local turndown_freq_words =
-{ 	["骚穴"] = { "saoxue", "saoxue", },
+{ 	["迪丽热吧"] = { "dilireba", },
 	["示~例~"] = { "shili", },
+	["骚穴"] = { "saoxue", "saoxue", },
 }
 return turndown_freq_words
